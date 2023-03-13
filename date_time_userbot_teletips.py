8e4fe1527f01c4bbc0f68b2b88be7c484201c792
Muhammadyoqub1031 <ymuxammadyoqub@mail.ru>
2023-03-13 10:38:28 +0500

    update

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -1,9 +1,3 @@
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
-#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [DATE_TIME Telegram userbot by TeLe TiPs] (https://github.com/teletips/DATE_TIME_USERBOT-TeLeTiPs)
-
-# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/DATE_TIME_USERBOT-TeLeTiPs/blob/main/LICENSE
-
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from lists_teletips.quotes_teletips import *
@@ -32,7 +26,7 @@ async def main_teletips():
                 Emojis_teletips = random.choice(emojis_teletips)
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
-                Date_teletips = TimeZone_teletips.strftime("%b %d") 
+                Date_teletips = TimeZone_teletips.strftime("%b %d")
                 Image_teletips = Image.open("image.jpg")
                 Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
                 Image_text_teletips = f"{Time_teletips}"
@@ -46,11 +40,11 @@ async def main_teletips():
                 try:
                     await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
                 except Exception:
-                    pass        
+                    pass
                 print("Profile Updated!")
-            await asyncio.sleep(60)     
+            await asyncio.sleep(60)
     except FloodWait as e:
-        await asyncio.sleep(e.x)         
+        await asyncio.sleep(e.x)
 
 print("DATE TIME USERBOT IS ALIVE!")
 asyncio.ensure_future(main_teletips())
