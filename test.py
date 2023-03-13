2881c2864d3bfdd9ed16b0087441dd7e1b045106
Muhammadyoqub1031 <ymuxammadyoqub@mail.ru>
2023-03-13 12:52:13 +0500

    aa

--- test.py
+++ test.py
@@ -2,13 +2,17 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from lists_teletips.quotes_teletips import *
 from lists_teletips.emojis_teletips import *
-from PIL import Image, ImageDraw, ImageFont
 import datetime
 import pytz
 import asyncio
 import random
 
-app = Client("my_account")
+app=Client(
+    name = "date_time_userbot_teletips",
+    api_id = '26163245',
+    api_hash = '8928271745e15652485f24c961c145c6',
+    session_string = "AgGPOC0AGeXHhykoZds7ELqmdAy-tSiJgrP5i5vcVyZxmqfdrqkH1fUxIR16w_MXoR4Ux6gu-VKuK6CLFLTiW5bgI4GBAL8IvPfD08xYLwipLEkEe6B0DA1De_6OVx0Ed-U7g8FfyXbHdZ6s8Ov8meBmxFn2Kh7OeTKq8iZ5wuB81FQpcGZhE6bmO2bdJW49izGlHJHi4Al5vqwwEIuL1ohoPOJAHl9wmV0bUgHcvJXjsFpDdxmfQAVbx6PfsCOl6Y9inynfPpDfczI1iyu51ApmoKpVFDfdzTk5570r5pVagZFqqXOl86hPaGfhOX7xK9n4qtDsZIEV-nY23k61s6_qhrMUbAAAAAFLb8KLAA"
+)
 
 Time_Zone = "Asia/Ashgabat"
 
@@ -21,22 +25,9 @@ async def main_teletips():
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
                 Date_teletips = TimeZone_teletips.strftime("%b %d")
-                Image_teletips = Image.open("image.jpg")
-                Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
-                Image_text_teletips = f"{Quotes_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
-                Image_teletips.save("Image_final_teletips.jpg")
                 await app.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏰ {Time_teletips} | 📅 {Date_teletips}")
-                await app.set_profile_photo(photo="Image_final_teletips.jpg")
-                me = await app.get_me()
-                photos = app.get_chat_photos("me")
-                try:
-                    await app.delete_profile_photos(photos[1].file_id)
-                except Exception:
-                    pass
                 print(f"Profile Yangilandi {Time_teletips}")
-            await asyncio.sleep(60)
+            await asyncio.sleep(20)
     except FloodWait as e:
         await asyncio.sleep(e.x)
 
